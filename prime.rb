57d71bc3ed4c34ded275da250029874e83958854
imtripleq <flamework89@hotmail.com>
2022-01-18 09:52:03 +1100

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+  if num == 0 or num < 0 or num == 1
+    return false
+  elsif num == 2
+    return true
+  else
+    (2..num - 1).to_a.all? { |factor| num % factor != 0 }
+  end
+end
